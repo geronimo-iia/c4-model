4d6af9e1061ac91d5a0a3d63c2d54e1a4fe2c76f
Jerome Guibert <jguibert@gmail.com>
2020-12-22 18:14:14 +0100

    add docstring

--- c4_model/definition.py
+++ c4_model/definition.py
@@ -13,14 +13,30 @@ PROVIDER_CODE = "c4"
 NAMESPACE_C4 = UUID("51d76b7f-57a7-4a63-9d88-3c2c0490e518")
 
 
-def get_resource_id(class_name: str, name: str):
-    """Returns a resource identifier."""
+def get_resource_id(class_name: str, name: str) -> UUID:
+    """Returns a resource identifier.
+
+    Arguments:
+        class_name (str): class name of model
+        name (str): name of the model
+
+    Returns:
+        (UUID): a resource identifier.
+    """
     return uuid3(NAMESPACE_C4, f"{class_name}:{name.replace(' ', '_').lower()}")
 
 
-def get_arn(resource_type, resource_id):
-    """Returns an arn."""
-    return f"arn:{PROVIDER_CODE}:{resource_type}:{resource_id }"
+def get_arn(resource_type: str, resource_id) -> str:
+    """Returns an arn.
+
+    Arguments:
+        resource_type (str): resource type
+        resource_id (str): resource identifier
+
+    Returns:
+        (str): arn.
+    """
+    return f"arn:{PROVIDER_CODE}:{resource_type}:{resource_id}"
 
 
 @dataclass
@@ -32,21 +48,48 @@ class Reference:
     resource_id: Optional[str] = None
     c4_class_name: InitVar[str] = None
 
-    def __post_init__(self, c4_class_name):
+    def __post_init__(self, c4_class_name: str):
+        """Post build class.
+
+        If c4_class_name is present, compute resource_type and resource_id memeber.
+
+        Arguments:
+            c4_class_name (str): optional c4_class_name
+        """
         if c4_class_name:
             self.resource_type = camel_to_snake(c4_class_name)
             self.resource_id = get_resource_id(class_name=c4_class_name, name=self.name)
 
     @property
     def arn(self) -> str:
+        """Returns arn.
+
+        Returns:
+            (str): arn of this instance
+        """
         return get_arn(resource_type=self.resource_type, resource_id=self.resource_id)
 
     @property
     def data(self) -> Dict:
+        """Returns a dictionnary of this instance.
+
+        Returns:
+            (Dict): dictionnary of this instance.
+        """
         return asdict(self)
 
-    def __eq__(self, other):
-        return self.resource_id == other.resource_id
+    def __eq__(self, other) -> bool:
+        """Compare two instance of Reference.
+
+        Arguments:
+            other: other instance to compare.
+
+        Returns:
+            (bool): true if resource_id are equals, false either.
+        """
+        if other:
+            return self.resource_id == other.resource_id if hasattr(other, "resource_id") else False
+        return False
 
 
 @dataclass
@@ -57,18 +100,38 @@ class BaseModel:
 
     @property
     def provider(self):
+        """Returns provider.
+
+        Returns:
+            (str): provider of this instance
+        """
         return PROVIDER_CODE
 
     @property
     def resource_type(self):
+        """Returns resource type.
+
+        Returns:
+            (str): resource type of this instance
+        """
         return camel_to_snake(self.__class__.__name__)
 
     @property
     def resource_id(self):
+        """Returns resource identifier.
+
+        Returns:
+            (str): resource identifierof this instance
+        """
         return get_resource_id(class_name=self.__class__.__name__, name=self.name)
 
     @property
     def data(self) -> Dict:
+        """Returns a dictionnary of this instance.
+
+        Returns:
+            (Dict): dictionnary of this instance.
+        """
         d = asdict(self)
         for (k, v) in d.items():
             if isinstance(v, Reference):
@@ -77,18 +140,46 @@ class BaseModel:
 
     @property
     def arn(self) -> str:
+        """Returns arn.
+
+        Returns:
+            (str): arn of this instance
+        """
         return get_arn(resource_type=self.resource_type, resource_id=self.resource_id)
 
-    def __hash__(self):
+    def __hash__(self) -> str:
+        """Compute hash.
+
+        Arguments:
+
+        Returns:
+           (str): a hash representation.
+        """
         return self.resource_id
 
     def __eq__(self, other):
+        """Compare two instance of BaseModel.
+
+        Arguments:
+            other: other instance to compare.
+
+        Returns:
+            (bool): true if resource_id are equals, false either.
+        """
         if other:
             return self.resource_id == other.resource_id if hasattr(other, "resource_id") else False
         return False
 
     @classmethod
     def from_resource(cls, data: Dict) -> "BaseModel":
+        """Instanciate a member of c4 model.
+
+        Arguments:
+            data (Dict): dictionnary of this instance.
+
+        Raises:
+            RuntimeError: base class always raise error.
+        """
         raise RuntimeError("Should not be called")
 
 
