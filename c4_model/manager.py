eb15bf29db73b5b4b658d5c99f35f5bd20d89944
Jerome Guibert <jguibert@gmail.com>
2020-12-22 18:14:51 +0100

    dont hide builin

--- c4_model/manager.py
+++ c4_model/manager.py
@@ -9,8 +9,8 @@ __all__ = ["C4Manager"]
 class C4Manager(UserDict):
     """C4Manager is an in memory dict of C4 Model."""
 
-    def __init__(self, dict=None):
-        super(C4Manager, self).__init__(dict=dict)
+    def __init__(self, data=None):
+        super(C4Manager, self).__init__(dict=data)
 
     def add(self, item: BaseModel):
         self.data[item.arn] = item
